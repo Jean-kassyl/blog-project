4d27bb1da767f4394137d7221eadae8c938eaf3c
Jean-kassyl <kassylmac@gmail.com>
2022-10-04 08:48:01 +0100

    feat: add the about page and the contact us page - fix: move the header and the footer in a partial folder - feat: style the new pages

--- index.js
+++ index.js
@@ -8,10 +8,28 @@ const homeContent = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. C
 maxime qui voluptatibus asperiores expedita magni illum debitis quidem? Nisi doloremque 
 fuga numquam reiciendis suscipit, nam quas eveniet at quis voluptatibus corrupti.`
 
+const aboutContent = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+Facilis praesentium eligendi atque excepturi accusantium. 
+Amet ab explicabo quaerat, itaque optio animi magnam quibusdam 
+vero voluptate repudiandae quos fuga quasi nostrum minima quisquam in. 
+Eveniet saepe consectetur voluptatibus esse eaque quam, voluptatum aliquam 
+quibusdam neque commodi recusandae odit fugit, nesciunt delectus illo 
+necessitatibus sit alias qui minima. Illo nemo repellat laudantium ullam, 
+impedit non cupiditate corporis architecto, ut commodi praesentium nihil 
+dolores a cum explicabo voluptates veniam similique sint? Modi, voluptas!`
+
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.render('index', {content: homeContent})
+})
+
+app.get('/about', function(req, res){
+    res.render('about', {content: aboutContent})
+})
+
+app.get('/contact', function(req, res){
+    res.render('contact')
 })
 const PORT = process.env.PORT || "3000"
 
